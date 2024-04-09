70721b9a2090608e9014fea86cfa54a0046bcb53
Pamela Sánchez <129999995+Pame-asj@users.noreply.github.com>
2024-04-08 23:23:56 -0400

    Add files via upload

---
+++ To do's/To do 16.sql
@@ -0,0 +1,11 @@
+SELECT ROUND(released_year, 0) AS año, title
+FROM movies
+GROUP BY released_year, title
+
+UNION ALL
+
+SELECT CONVERT(int, SUBSTRING(Year,1,4)) AS año, Title
+FROM series
+
+GROUP BY Year, Title;
+
