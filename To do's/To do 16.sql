db42d6ddf8a7bb10d80259b3eb186d98bde111bc
Noch-Voron <116518720+Noch-Voron@users.noreply.github.com>
2024-04-13 11:36:52 -0400

    Update To do 16.sql
    ahora agrupa en una misma celda los titulos de un mismo año

--- To do's/To do 16.sql
+++ To do's/To do 16.sql
@@ -1,3 +1,5 @@
+select year,
+    string_agg(title, ', ') as titles from (
 select
     coalesce(movies.title, series.title) as title,
     coalesce(movies.released_year, left(series.Year, 4)) as year
@@ -6,4 +8,6 @@ full outer join series
     on movies.title = series.title and movies.released_year = left(series.Year, 4)
 where 
     (movies.released_year is not null or series.Year is not null)
+) as SubQuery
+group by year
 order by year;
