5c8db0ddf719bf2551bb18223e2bff0bb1498e7d
Noch-Voron <116518720+Noch-Voron@users.noreply.github.com>
2024-04-13 11:20:25 -0400

    Update To do 16.sql
    Ordena ambas tablas correctamente

--- To do's/To do 16.sql
+++ To do's/To do 16.sql
@@ -1,12 +1,9 @@
-SELECT ROUND(released_year, 0) AS año, title
-FROM movies
-GROUP BY released_year, title
-HAVING released_year IS NOT NULL
-  
-UNION ALL
-
-SELECT CONVERT(int, SUBSTRING(Year,1,4)) AS año, Title
-FROM series
-
-GROUP BY Year, Title
-HAVING Year IS NOT NULL;
+select
+    coalesce(movies.title, series.title) as title,
+    coalesce(movies.released_year, left(series.Year, 4)) as year
+from movies
+full outer join series
+    on movies.title = series.title and movies.released_year = left(series.Year, 4)
+where 
+    (movies.released_year is not null or series.Year is not null)
+order by year;
