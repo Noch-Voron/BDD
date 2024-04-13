1aeeaad60c611a24b97b5f3ff67fb61b37a2f3d3
Pamela Sánchez <129999995+Pame-asj@users.noreply.github.com>
2024-04-13 01:32:27 -0400

    Update To do 17.sql

--- To do's/To do 17.sql
+++ To do's/To do 17.sql
@@ -1,11 +1,3 @@
-SELECT genre as Tipo, title
-FROM movies
-where genre like '%Animation%'
-GROUP BY title, genre
-
-UNION ALL
-
-SELECT Technique as Tipo, Title
-FROM series
-
-GROUP BY Title, Technique;
+SELECT Titulo, Genero, Tecnica
+FROM AnimationTable
+ORDER BY Titulo ASC;
