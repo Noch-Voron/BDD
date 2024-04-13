1dfe0da55c00afec6173e204e94d5c6d2170f9b1
Pamela Sánchez <129999995+Pame-asj@users.noreply.github.com>
2024-04-13 01:46:33 -0400

    Update CrearTablaAnimation.sql

--- To do's/CrearTablaAnimation.sql
+++ To do's/CrearTablaAnimation.sql
@@ -1,10 +1,10 @@
 CREATE TABLE AnimationTable (
 	Id INT IDENTITY(1,1) PRIMARY KEY,
-    Titulo varchar(100),
-    Genero varchar(200),
-    Tecnica varchar(200),
-    IMDb float,
-	Año INT,
+	Titulo varchar(100),
+	Genero varchar(200),
+	Tecnica varchar(200),
+	IMDb float,
+	AÃ±o INT,
 	Episodios float,
 	runtime varchar(20),
 	rated_class varchar(20),
@@ -14,11 +14,11 @@ CREATE TABLE AnimationTable (
 	Id_series INT FOREIGN KEY REFERENCES series(Id)
 );
 
-INSERT INTO AnimationTable(Titulo, Año, Genero, IMDb, runtime, rated_class, total_ratings, summary, Id_movies)
+INSERT INTO AnimationTable(Titulo, AÃ±o, Genero, IMDb, runtime, rated_class, total_ratings, summary, Id_movies)
 SELECT title, ROUND(released_year, 0), genre, stars, runtime, rated_class, total_ratings, summary, Id
 FROM movies
 WHERE genre LIKE '%Animation%';
 
-INSERT INTO AnimationTable(Titulo, Año, Tecnica, IMDb, Episodios, Id_series)
+INSERT INTO AnimationTable(Titulo, AÃ±o, Tecnica, IMDb, Episodios, Id_series)
 SELECT title, CONVERT(int, SUBSTRING(Year,1,4)), Technique, IMDb, Episodes, Id
 FROM series;
